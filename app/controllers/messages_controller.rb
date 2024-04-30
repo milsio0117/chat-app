57ba88d4a1c8999fec80a837dee3f88bce069b7c
milsio0117 <milsio117@gmail.com>
2024-04-30 14:38:23 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,29 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
+    
+  end
+
+  # def destroy
+  #   room = Room.find(params[:id])
+  #   room.destroy
+  #   redirect_to root_path
+  # end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
